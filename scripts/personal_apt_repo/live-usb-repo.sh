2ad6feef4dff04868b00fb8b27da21f51cef4af5
mezcel <mezcel@hotmail.com>
2021-02-08 09:09:46 -0500

    added extra notes

--- scripts/personal_apt_repo/live-usb-repo.sh
+++ scripts/personal_apt_repo/live-usb-repo.sh
@@ -5,3 +5,12 @@
 mkdir -p /etc/apt/sources.list.d/
 
 echo "deb [trusted=yes] file:///usr/lib/live/mount/medium downloaded-debs/" >> /etc/apt/sources.list.d/myRepoMirror.list
+
+## Make list of installed applications
+# mkdir ~/Downloads
+# dpkg -l | grep ^ii | awk '{print $2}' | cut -d: -f1 > ~/Downloads/live-downloads.txt
+
+## prepend "sudo apt install" on every line
+#echo '#!/bin/bash' > ~/Downloads/live-installs.txt
+#echo "" >> ~/Downloads/live-installs.txt
+#sed 's/^/sudo apt install /g' ~/Downloads/live-downloads.txt >> ~/Downloads/live-installs.txt

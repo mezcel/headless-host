78168f0ab9eac70eb626704651145f3fdf70e684
mezcel <mezcel@hotmail.com>
2021-04-16 05:21:41 -0500

    update installer

--- scripts/wsl-files/install-cyg-packages.sh
+++ scripts/wsl-files/install-cyg-packages.sh
@@ -7,61 +7,80 @@ function install_apt_cyg {
 	## terminal package manager
 
 	if [ ! -f ~/apt-cyg ]; then
-		wget -O ~/apt-cyg https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg
+	
+		command -v wget
+		
+		if [ $? -ne 0 ] then
+			echo -e "\n# wget is not installed to download https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg\n"
+			exit
+		else
+			wget -O ~/apt-cyg https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg
+		fi
+		
+		sleep 1s
 	fi
 
 	if [ -f ~/apt-cyg ]; then
 		chmod +x ~/apt-cyg
+		sleep 1s
 	fi
 }
 
+function AptCyg {
+	packageName=$1
+	bash ~/apt-cyg install $packageName
+	sleep 2s
+	
+}
 
 function install_packages {
-	apt-cyg install gcc-core
-	apt-cyg install bash 
-	apt-cyg install binutils 
-	apt-cyg install cygwin 
-	apt-cyg install cygwin-devel 
-	apt-cyg install libatomic1 
-	apt-cyg install libgcc1 
-	apt-cyg install libgmp10 
-	apt-cyg install libgomp1 
-	apt-cyg install libiconv2 
-	apt-cyg install libintl8 
-	apt-cyg install libisl22 
-	apt-cyg install libmpc3 
-	apt-cyg install libmpfr6 
-	apt-cyg install libquadmath0 
-	apt-cyg install libzstd1 
-	apt-cyg install w32api-headers 
-	apt-cyg install w32api-runtime 
-	apt-cyg install windows-default-manifest 
-	apt-cyg install zlib0
+
+	AptCyg gcc-core
+	AptCyg bash 
+	AptCyg binutils 
+	AptCyg cygwin 
+	AptCyg cygwin-devel 
+	AptCyg libatomic1 
+	AptCyg libgcc1 
+	AptCyg libgmp10 
+	AptCyg libgomp1 
+	AptCyg libiconv2 
+	AptCyg libintl8 
+	AptCyg libisl22 
+	AptCyg libmpc3 
+	AptCyg libmpfr6 
+	AptCyg libquadmath0 
+	AptCyg libzstd1 
+	AptCyg w32api-headers 
+	AptCyg w32api-runtime 
+	AptCyg windows-default-manifest 
+	AptCyg zlib0
 	
-	apt-cyg install cygutils-extra
-	apt-cyg install cygwin-debuginfo
+	AptCyg cygutils-extra
+	AptCyg cygwin-debuginfo
 
 	## apps
-	apt-cyg install dos2unix 
-	apt-cyg install vim 
-	apt-cyg install wget 
-	apt-cyg install git
-	apt-cyg install tmux
+	AptCyg dos2unix 
+	AptCyg vim 
+	AptCyg wget 
+	AptCyg git
+	AptCyg tmux
+	AptCyg highlight-common
 
 	## python
 	#apt-cyg install python
-	apt-cyg install python-pip 
-	apt-cyg install python3 
-	apt-cyg install python3-pip
+	AptCyg python-pip 
+	AptCyg python3 
+	AptCyg python3-pip
 	
-	apt-cyg install mc
+	AptCyg mc
 	
 	## vifm depends
 	## https://wiki.vifm.info/index.php/Obtaining_Vifm#Cygwin
 	## There are no prebuild Cygwin packages for Vifm, so one needs to build it from sources.
-	apt-cyg install make
-	apt-cyg install gcc-core
-	apt-cyg install ncurses-devel
+	AptCyg make
+	AptCyg gcc-core
+	AptCyg ncurses-devel
 	
 	#mkdir -p ~/Downloads/
 	#wget -O ~/Downloads/vifm-0.11.tar.bz2 http://prdownloads.sourceforge.net/vifm/vifm-0.11.tar.bz2?download
@@ -70,14 +89,12 @@ function install_packages {
 	#rm -rf ~/.config/vifm/colors
 	#git clone https://github.com/vifm/vifm-colors ~/.config/vifm/colors
 	
-	## Misc
-	apt-cyg install libjson-c-devel
-	apt-cyg install libjson-devel
-	apt-cyg install libjson-c2
+	## printf-time.git depends
+	AptCyg libjson-c-devel
+	AptCyg libjson-devel
+	AptCyg libjson-c2
 	
-	apt-cyg install highlight-common
-	
-	json-c-debuginfo
+	AptCyg json-c-debuginfo
 }
 
 install_apt_cyg 
